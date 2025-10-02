ad0fb7d89f5170e4bcef544624af86651e3d4a5f
rsuntk <rsuntk@yukiprjkt.my.id>
2025-10-02 16:39:26 +0700

    kernel: make apk_sign_key as a typedef instead
    Style preference.
    
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -25,4 +25,9 @@
 #define EXPECTED_SIZE_OTHER   0x300
 #define EXPECTED_HASH_OTHER   "0000000000000000000000000000000000000000000000000000000000000000"
 
+typedef struct {
+	unsigned size;
+	const char *sha256;
+} apk_sign_key_t;
+
 #endif /* MANAGER_SIGN_H */
